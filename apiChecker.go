5187b6c1dcd00d5cd1bbfa6db5b86b44c443b11c
Serban Murariu <Serban.Murariu@sundiogroup.com>
2018-02-26 15:52:24 +0100

    Added playerChecks

---
+++ apiChecker.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"./playerCheck"
+	"github.com/influxdata/influxdb/client/v2"
+)
+
+const (
+	myDB     = "db0"
+	username = "apiChecker"
+	password = "SundioApiChecker1234"
+)
+
+func main() {
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     "http://10.10.18.167:8086",
+		Username: username,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ticker := time.NewTicker(5000 * time.Millisecond)
+	for t := range ticker.C {
+		fmt.Print(t)
+		results := playerCheck.ExecuteChecks()
+		writeToInflux(c, results)
+		fmt.Printf(" %s\n", results)
+	}
+}
+
+func writeToInflux(c client.Client, results [2]playerCheck.Execution) {
+
+	// Create a new point batch
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  myDB,
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a point and add to batch
+	tags := map[string]string{"env": "ACPT"}
+	fields := map[string]interface{}{
+		results[0].Name: int64(results[0].Duration / time.Millisecond),
+		results[1].Name: int64(results[1].Duration / time.Millisecond),
+	}
+
+	pt, err := client.NewPoint("Player", tags, fields, time.Now())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := c.Write(bp); err != nil {
+		log.Fatal(err)
+	}
+}
